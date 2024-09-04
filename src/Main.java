c613db96f6e3717bf0859fd02349a668d80ecb43
Samuel Arseneault <Samor140@hotmail.com>
2024-09-04 13:36:06 -0400

    commit initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        GameWindow game = new GameWindow();
+        game.start();
     }
 }
