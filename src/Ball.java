5722053359e9570b8df3cf7dc1705b3a0c223d31
Samuel Arseneault <158058364+sam0rr@users.noreply.github.com>
2024-09-18 13:30:59 -0400

    Add files via upload

--- src/Ball.java
+++ src/Ball.java
@@ -3,64 +3,62 @@ import java.util.Random;
 
 public class Ball {
 
-
     public static final int DEFAULT_SPEED = 5;
-    private int radius;
-    private int speed;
+
     private int x;
     private int y;
     private int dx;
     private int dy;
+    private int speed;
+    private int radius;
 
-
-    public Ball(int radius, int speed) {
+    public Ball(int radius, int speed){
         this.radius = radius;
         this.speed = speed;
         initializePosition();
     }
 
-    public Ball(int radius) {
-        this(radius, DEFAULT_SPEED);
+    public Ball(int radius){
+        this(radius,DEFAULT_SPEED);
     }
 
     public void update(){
         x += dx;
         y += dy;
-        if(hasTouchVertical()){
+        if(y <= radius || y >= 600-radius){
             dy *= -1;
         }
-        if(hasTouchHorizontal()){
+        if(x <= radius || x >= 800-radius){
             dx *= -1;
         }
     }
 
-    public void draw(Graphics2D bufferEngine) {
-        bufferEngine.setPaint(Color.YELLOW);
-        bufferEngine.fillOval(x,y,radius * 2,radius * 2);
+    public void draw(Canvas bufferEngine)
+    {
+        bufferEngine.drawCircle(x,y,radius * 2, Color.red);
     }
 
-    private boolean hasTouchVertical() {
-        return y <= radius || y >= 600 - radius;
+    public boolean hasTouched(){
+        return hasTouchedHorizontal()
+                || hasTouchedVertical();
     }
 
-    private boolean hasTouchHorizontal() {
-        return x <= radius || x >= 800 - radius;
-    }
-
-    public boolean hasTouched() {
-        return hasTouchVertical()
-                || hasTouchHorizontal();
-    }
-
-    private void initializePosition() {
+    private void initializePosition(){
         x = randomNumber(radius * 2, 800 - radius * 2);
         y = randomNumber(radius * 2, 600 - radius * 2);
-        dx = randomNumber(0,1) == 0 ? speed : - speed;
-        dy = randomNumber(0,1) == 0 ? speed : - speed;
-
+        dx = randomNumber(0,1) == 0 ? speed : -speed;
+        dy = randomNumber(0,1) == 0 ? speed : -speed;
     }
 
     private int randomNumber(int min, int max) {
-        return  (new Random()).nextInt((max - min) + 1) + min;
+        return (new Random()).nextInt((max - min) + 1) + min;
+    }
+
+    private boolean hasTouchedVertical(){
+        return y <= radius || y >= 600-radius;
+    }
+
+    private boolean hasTouchedHorizontal(){
+        return x <= radius || x >= 600-radius;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
-        GameWindow game = new GameWindow();
-        game.start();
+        BouncingBallGame bouncingBallGame = new BouncingBallGame();
+        bouncingBallGame.start();
     }
 }
