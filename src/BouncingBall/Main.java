eda11c18867d69c2971b299863e1980f38a469ec
Samuel Arseneault <Samor140@hotmail.com>
2024-10-02 12:38:31 -0400

    test github fix

--- src/BouncingBall/Main.java
+++ src/BouncingBall/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         BouncingBallGame bouncingBallGame = new BouncingBallGame();
         bouncingBallGame.start();
+        //adding comment in main to tests
     }
 }
